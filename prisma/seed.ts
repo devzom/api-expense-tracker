ee52098ff926e9fa5e78e91f6d0b8668a8e9fda8
Jakub Zomerfeld <jakub.zomerfeld@gmail.com>
2025-01-19 11:35:14 +0100

    feat: Seed extend for users,paymentMethods and expensesCategories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,80 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
-    const alice = await prisma.user.upsert({
-        where: { email: 'alice@prisma.io' },
-        update: {},
-        create: {
-            email: 'alice@prisma.io',
-            name: 'Alice',
-        },
-    })
-    const bob = await prisma.user.upsert({
-        where: { email: 'bob@prisma.io' },
-        update: {},
-        create: {
-            email: 'bob@prisma.io',
-            name: 'Bob',
-        },
-    })
-    console.log({ alice, bob })
+
+    const users = await Promise.all([
+        prisma.user.upsert({
+            where: { email: 'bob@prisma.io' },
+            update: {},
+            create: {
+                email: 'bob@prisma.io',
+                name: 'Bob',
+                expenses: {
+                    create: {
+                        amount: 100,
+                        description: 'Pizza',
+                        category: {
+                            connect: {
+                                name: 'food'
+                            }
+                        },
+                        paymentMethod: {
+                            connect: {
+                                name: 'Card'
+                            }
+                        }
+                    }
+                },
+            }
+        }),
+        prisma.user.upsert({
+            where: { email: 'alice@prisma.io' },
+            update: {},
+            create: {
+                email: 'alice@prisma.io',
+                name: 'Alice',
+            },
+        })
+    ])
+
+    const paymentsMethod = await Promise.all([
+        prisma.paymentMethod.upsert({
+            where: { name: 'Card' },
+            update: {},
+            create: {
+                name: 'Card',
+            },
+        }),
+        prisma.paymentMethod.upsert({
+            where: { name: 'Cash' },
+            update: {},
+            create: {
+                name: 'Cash',
+            },
+        })
+    ])
+
+    const expensesCategories = await Promise.all([
+        prisma.category.upsert({
+            where: { name: 'food' },
+            update: {},
+            create: {
+                name: 'food',
+            },
+        }),
+        prisma.category.upsert({
+            where: { name: 'bills' },
+            update: {},
+            create: {
+                name: 'bills',
+            },
+        })
+    ])
+
+
+
+
+    console.log(users, paymentsMethod, expensesCategories)
 }
 main()
     .then(async () => {
