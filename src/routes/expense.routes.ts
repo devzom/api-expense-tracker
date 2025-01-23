3d082e0eb9c8ee52f36508b839c060dc50bf62e6
Jakub Zomerfeld <jakub.zomerfeld@gmail.com>
2025-01-23 21:03:42 +0100

    fix: invalid route param for userId used in resourceAccessMiddleware

--- src/routes/expense.routes.ts
+++ src/routes/expense.routes.ts
@@ -1,19 +1,19 @@
 import { Hono } from "hono";
 import { createExpense, updateExpense, deleteExpense, getExpense, getUserExpenses, getUserSummary } from "../modules/expense";
 import { resourceAccessMiddleware } from "../middlewares/auth";
+import { userIdentifier } from "../constans";
 
 const app = new Hono(
     { strict: true }
 );
 
-app.use("*", resourceAccessMiddleware);
-
-app.get("/:id", getExpense);
-app.post("/", createExpense);
-app.put("/:id", updateExpense);
-app.delete("/:id", deleteExpense);
+// Routes that need user verification
+app.get(`/users/:${userIdentifier}/expenses/:id`, resourceAccessMiddleware, getExpense);
+app.post(`/users/:${userIdentifier}/expenses`, resourceAccessMiddleware, createExpense);
+app.put(`/users/:${userIdentifier}/expenses/:id`, resourceAccessMiddleware, updateExpense);
+app.delete(`/users/:${userIdentifier}/expenses/:id`, resourceAccessMiddleware, deleteExpense);
 
 // Get user expenses with pagination and filters
-app.get("/users/:user", getUserExpenses);
-app.get("/users/:user/summary", getUserSummary);
+app.get(`/users/:${userIdentifier}`, resourceAccessMiddleware, getUserExpenses);
+app.get(`/users/:${userIdentifier}/summary`, resourceAccessMiddleware, getUserSummary);
 export default app;
